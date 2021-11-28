e963ddeec3572b28cf5edaef2efaef91ffd0e703
Larry Lewis <lel486@gmail.com>
2021-11-28 15:32:36 -0500

    add code...

--- ResampleStockData/ResampleStockData.cpp
+++ ResampleStockData/ResampleStockData.cpp
@@ -7,19 +7,46 @@
 using std::cout;
 using std::endl;
 using std::string;
+bool processCommandLine(int argc, const char* argv[], std::filesystem::path& directory, char& interval, std::vector<int>& ratio);
 
 int main(int argc, const char* argv[])
 {
     // process command line arguments;
-    std::string directory;
+    std::filesystem::path directory;
+    char interval;
+    std::vector<int> ratio{ 0, 0, 0 };
+    bool rc = processCommandLine(argc, argv, directory, interval, ratio);
+    if (!rc)
+        return -1;
+
+    // process each file in directory
+    std::vector<std::string> file_list;
+    for (const auto& entry : std::filesystem::directory_iterator(directory))
+    {
+        const auto full_name = entry.path().string();
+        if (entry.is_regular_file())
+        {
+            if (entry.path().extension().string() != "csv")
+                continue;
+            const string base_name = entry.path().filename().string();
+            file_list.push_back(full_name);
+        }
+    }
+
+    return 0;
+}
+
+bool processCommandLine(int argc, const char* argv[], std::filesystem::path& directory, char& interval, std::vector<int>& ratio) {
     bool directorySpecified = false;
-    for (int i = 0; i < argc; ++i) {
+    bool intervalIsSpecified = false;
+    bool ratioIsSpecified = false;
+    for (int i = 1; i < argc; ++i) {
         const string parm(argv[i]);
         if (parm == "-d" || parm == "--directory") {
             if (directorySpecified)
             {
-                cout << "***Error*** -d specified more than once." << endl;
-                return -1;
+                cout << "***Error*** directory specified more than once." << endl;
+                return false;
             }
             directorySpecified = true;
             if (i < argc) {
@@ -28,47 +55,35 @@ int main(int argc, const char* argv[])
                 std::filesystem::path folder(directory);
                 if (!std::filesystem::is_directory(folder)) {
                     cout << "***Error*** specified directory does not exist or is not actually a directory." << endl;
-                    return - 1;
+                    return false;
                 }
             }
             else {
                 cout << "***Error*** No directory specified after -d" << endl;
-                return -1;
+                return false;
             }
         }
         else if (parm == "-i" || parm == "--interval") {
+            if (intervalIsSpecified)
+            {
+                cout << "***Error*** interval specified more than once." << endl;
+                return false;
+            }
+            intervalIsSpecified = true;
         }
         else if (parm == "-r" || parm == "--ratio") {
+            if (ratioIsSpecified)
+            {
+                cout << "***Error*** ratio specified more than once." << endl;
+                return false;
+            }
+            ratioIsSpecified = true;
         }
         else {
             cout << "'" << parm << "' is not a recognized parameter. Try ResampleStockData.cpp -h" << endl;
+            return false;
         }
     }
 
-    std::filesystem::path folder(directory);
-
-    std::vector<std::string> file_list;
-    for (const auto& entry : std::filesystem::directory_iterator(folder))
-    {
-        const auto full_name = entry.path().string();
-
-        if (entry.is_regular_file())
-        {
-            const auto base_name = entry.path().filename().string();
-            file_list.push_back(full_name);
-        }
-    }
-
-    return 0;
+    return true;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
